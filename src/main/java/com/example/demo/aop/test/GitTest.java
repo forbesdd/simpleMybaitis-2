4bcd78e13343d49b94c7c0c86ca4647fc7054217
forbes <forbes.david1828@gmail.com>
2023-07-07 11:20:51 +0900

    Merge branch 'master' into change01
    # Conflicts:
    #	src/main/java/com/example/demo/controller/EmpController.java
    #	src/main/java/com/example/demo/controller/LoginController.java
    #	src/main/java/com/example/demo/controller/UploadController.java
    #	src/main/java/com/example/demo/mapper/EmpMapper.java
    #	src/main/java/com/example/demo/pojo/Dept.java
    #	src/main/java/com/example/demo/pojo/Emp.java
    #	src/main/java/com/example/demo/pojo/Result.java
    #	src/main/java/com/example/demo/service/impl/DeptServiceImpl.java

---
+++ src/main/java/com/example/demo/aop/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.demo.aop.test;
+
+/**
+ * @Author:david
+ * @Date:2023/07/07 - 07 - 07
+ * @Description:com.example.demo.aop.test Version:1.0
+ */
+public class GitTest {
+    private String name;
+
+}
